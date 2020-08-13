e765900d1c9febe8533b50ea7d4cadb257d344c6
Kronopt <Kronopt@users.noreply.github.com>
2020-08-13 23:04:53 +0100

    release: 0.2.2

--- xkcd_wrapper/__init__.py
+++ xkcd_wrapper/__init__.py
@@ -27,7 +27,7 @@ Async Usage:
 __author__ = 'Pedro HC David, https://github.com/Kronopt'
 __credits__ = ['Pedro HC David']
 __license__ = 'GPLv3'
-__version__ = '0.2.2a'
+__version__ = '0.2.2'
 
 
 REQUESTS_INSTALLED = True
