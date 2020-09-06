34638219ee5cc14be2375243723d6eef17d0ed45
Kronopt <Kronopt@users.noreply.github.com>
2020-09-06 18:04:19 +0100

    test: new and reworked tests

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -12,7 +12,7 @@ import unittest
 import xkcd_wrapper
 from aioresponses import aioresponses
 from nose2.tools.params import params as nose2_params
-from . import (base_url, latest_comic_url, comic_id_url, check_comic,
+from . import (base_url, latest_comic_url, comic_id_url, check_comic, raw_comic_image,
                xkcd_api_example_628_raw,
                xkcd_api_example_138_raw,
                xkcd_api_example_wrong_raw,
@@ -31,6 +31,13 @@ class TestClient(unittest.TestCase):
         self.assertIsInstance(c._base_url, str)
         self.assertIsInstance(c._api, str)
         self.assertIsInstance(c._explanation_wiki_url, str)
+        self.assertIsInstance(c._explanation_wiki_url, str)
+
+        self.assertIsInstance(c._response_int_values, dict)
+        self.assertEqual(c._response_int_values['num'], 'id')
+        self.assertEqual(c._response_int_values['year'], 'date')
+        self.assertEqual(c._response_int_values['month'], 'date')
+        self.assertEqual(c._response_int_values['day'], 'date')
 
     def test_base_url(self):
         c = xkcd_wrapper.AsyncClient()
@@ -50,8 +57,21 @@ class TestClient(unittest.TestCase):
         loop = asyncio.get_event_loop()
         with aioresponses() as mock:
             mock.get(comic_id_url.format(628), status=200, body=xkcd_api_example_628_raw)
+            mock.get(xkcd_api_example_628_dict['img'], status=200, body=raw_comic_image)
             response = loop.run_until_complete(c.get(628))
             check_comic(self, response, xkcd_api_example_628_dict)
+            with self.assertRaises(TypeError):
+                loop.run_until_complete(c.get(''))
+            with self.assertRaises(TypeError):
+                loop.run_until_complete(c.get([1, 2, 3]))
+
+    def test_get_without_raw_image(self):
+        c = xkcd_wrapper.AsyncClient()
+        loop = asyncio.get_event_loop()
+        with aioresponses() as mock:
+            mock.get(comic_id_url.format(628), status=200, body=xkcd_api_example_628_raw)
+            response = loop.run_until_complete(c.get(628, raw_comic_image=False))
+            check_comic(self, response, xkcd_api_example_628_dict, raw_image=False)
             with self.assertRaises(TypeError):
                 loop.run_until_complete(c.get(''))
             with self.assertRaises(TypeError):
@@ -63,7 +83,7 @@ class TestClient(unittest.TestCase):
         with aioresponses() as mock:
             mock.get(comic_id_url.format(628), status=404)
             with self.assertRaises(xkcd_wrapper.exceptions.HttpError):
-                loop.run_until_complete(c.get(628))
+                loop.run_until_complete(c.get(628, raw_comic_image=False))
 
     def test_get_bad_response_fields(self):
         c = xkcd_wrapper.AsyncClient()
@@ -71,20 +91,20 @@ class TestClient(unittest.TestCase):
         with aioresponses() as mock:
             mock.get(comic_id_url.format(628), status=200, body=xkcd_api_example_wrong_raw)
             with self.assertRaises(xkcd_wrapper.exceptions.BadResponseField):
-                loop.run_until_complete(c.get(628))
+                loop.run_until_complete(c.get(628, raw_comic_image=False))
 
     def test_get_latest(self):  # let's assume the example_628 json is the latest comic
         c = xkcd_wrapper.AsyncClient()
         loop = asyncio.get_event_loop()
         with aioresponses() as mock:
             mock.get(latest_comic_url, status=200, body=xkcd_api_example_628_raw)
-            response = loop.run_until_complete(c.get_latest())
-            check_comic(self, response, xkcd_api_example_628_dict)
+            response = loop.run_until_complete(c.get_latest(raw_comic_image=False))
+            check_comic(self, response, xkcd_api_example_628_dict, raw_image=False)
 
             # alias
             mock.get(latest_comic_url, status=200, body=xkcd_api_example_628_raw)
-            response = loop.run_until_complete(c.latest())
-            check_comic(self, response, xkcd_api_example_628_dict)
+            response = loop.run_until_complete(c.latest(raw_comic_image=False))
+            check_comic(self, response, xkcd_api_example_628_dict, raw_image=False)
 
     def test_get_random(self):  # let's assume the example_628 json is the latest comic
         c = xkcd_wrapper.AsyncClient()
@@ -93,17 +113,17 @@ class TestClient(unittest.TestCase):
             mock.get(latest_comic_url, status=200, body=xkcd_api_example_628_raw)
             mock.get(comic_id_url.format(138), status=200, body=xkcd_api_example_138_raw)
             random.seed(1)  # with latest comic being 628, random value will be 138
-            response = loop.run_until_complete(c.get_random())
+            response = loop.run_until_complete(c.get_random(raw_comic_image=False))
             self.assertEqual(response.id, xkcd_api_example_138_dict['num'])
-            check_comic(self, response, xkcd_api_example_138_dict)
+            check_comic(self, response, xkcd_api_example_138_dict, raw_image=False)
 
             # alias
             mock.get(latest_comic_url, status=200, body=xkcd_api_example_628_raw)
             mock.get(comic_id_url.format(138), status=200, body=xkcd_api_example_138_raw)
             random.seed(1)
-            response = loop.run_until_complete(c.get_random())
+            response = loop.run_until_complete(c.get_random(raw_comic_image=False))
             self.assertEqual(response.id, xkcd_api_example_138_dict['num'])
-            check_comic(self, response, xkcd_api_example_138_dict)
+            check_comic(self, response, xkcd_api_example_138_dict, raw_image=False)
 
     def test__repr__(self):
         c = xkcd_wrapper.AsyncClient()
--- tests/test_comic.py
+++ tests/test_comic.py
@@ -8,7 +8,7 @@ Tests for xkcd_wrapper.Comic
 import datetime
 import unittest
 import xkcd_wrapper
-from nose2.tools.params import params as nose2_params
+from . import comic_id_url, explanation_wiki_url, raw_comic_image
 
 
 test_dict = {
@@ -25,59 +25,107 @@ test_dict = {
     'day':          10,
 }
 test_dict_missing_values = {
-    'link': '',
-    'year': 2020,
-    'news': '',
-    'title': 'test unused title',
-    'day': 10,
+    'unexpected_value': '',
 }
-
-raw_image = b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90wS\xde\x00\x00\x00\x01sRGB\x00\xae\xce\x1c\xe9\x00\x00\x00\x04gAMA\x00\x00\xb1\x8f\x0b\xfca\x05\x00\x00\x00\tpHYs\x00\x00\x16%\x00\x00\x16%\x01IR$\xf0\x00\x00\x00\x0cIDAT\x18Wc\xf8\xff\xff?\x00\x05\xfe\x02\xfe\xa75\x81\x84\x00\x00\x00\x00IEND\xaeB`\x82'
-comic_url = 'http://test_comic_url'
-explanation_url = 'http://test_explanation_url'
 
 
 class TestComic(unittest.TestCase):
 
-    @nose2_params(test_dict, test_dict_missing_values)
-    def test_comic(self, xkcd_dict):
-        c = xkcd_wrapper.Comic(xkcd_dict, raw_image, comic_url, explanation_url)
+    def test_comic_ok(self):
+        c = xkcd_wrapper.Comic(test_dict, raw_comic_image, comic_id_url, explanation_wiki_url)
         self.assertIsInstance(c, xkcd_wrapper.Comic)
 
         # id
-        self.assertEqual(c.id, xkcd_dict.get('num'))
+        self.assertEqual(c.id, test_dict.get('num'))
 
         # date
-        if all([xkcd_dict.get('year'), xkcd_dict.get('month'), xkcd_dict.get('day')]):
-            self.assertEqual(c.date, datetime.date(2020, 2, 10))
-        else:
-            self.assertIsNone(c.date)
+        self.assertEqual(c.date, datetime.date(2020, 2, 10))
 
         # title
-        self.assertEqual(c.title, xkcd_dict.get('safe_title'))
+        self.assertEqual(c.title, test_dict.get('safe_title'))
 
         # description
-        self.assertEqual(c.description, xkcd_dict.get('alt'))
+        self.assertEqual(c.description, test_dict.get('alt'))
 
         # transcript
-        self.assertEqual(c.transcript, xkcd_dict.get('transcript'))
+        self.assertEqual(c.transcript, test_dict.get('transcript'))
 
         # image
-        self.assertEqual(c.image, raw_image)
+        self.assertEqual(c.image, raw_comic_image)
 
         # image_extension
         self.assertEqual(c.image_extension, 'png')
 
         # image_url
-        self.assertEqual(c.image_url, xkcd_dict.get('img'))
+        self.assertEqual(c.image_url, test_dict.get('img'))
 
         # comic_url
-        self.assertEqual(c.comic_url, comic_url)
+        self.assertEqual(c.comic_url, comic_id_url)
 
         # explanation_url
-        self.assertEqual(c.explanation_url, explanation_url)
+        self.assertEqual(c.explanation_url, explanation_wiki_url)
 
-        if c.id:
-            self.assertEqual(str(c), 'xkcd_wrapper.Comic(10)')
-        else:
-            self.assertEqual(str(c), 'xkcd_wrapper.Comic(None)')
+        # __repr__
+        self.assertEqual(str(c), 'xkcd_wrapper.Comic(10)')
+
+    def test_comic_missing_values(self):
+        c = xkcd_wrapper.Comic(test_dict_missing_values)
+        self.assertIsInstance(c, xkcd_wrapper.Comic)
+
+        # id
+        self.assertIsNone(c.id)
+
+        # date
+        self.assertIsNone(c.date)
+
+        # title
+        self.assertIsNone(c.title)
+
+        # description
+        self.assertIsNone(c.description)
+
+        # transcript
+        self.assertIsNone(c.transcript)
+
+        # image
+        self.assertIsNone(c.image)
+
+        # image_extension
+        self.assertIsNone(c.image_extension)
+
+        # image_url
+        self.assertIsNone(c.image_url)
+
+        # comic_url
+        self.assertIsNone(c.comic_url)
+
+        # explanation_url
+        self.assertIsNone(c.explanation_url)
+
+        # __repr__
+        self.assertEqual(str(c), 'xkcd_wrapper.Comic(None)')
+
+    def test_update_raw_image(self):
+        c = xkcd_wrapper.Comic(test_dict)
+
+        self.assertIsNone(c.image)
+        self.assertIsNone(c.image_extension)
+
+        c.update_raw_image(raw_comic_image)
+
+        self.assertEqual(c.image, raw_comic_image)
+        self.assertEqual(c.image_extension, 'png')
+
+    def test__determine_date(self):
+        ok = {'year': 2020, 'month': 2, 'day': 10}
+        missing_one = {'year': 2020, 'month': 2}
+        missing_all = {}
+
+        self.assertEqual(xkcd_wrapper.Comic._determine_date(ok), datetime.date(2020, 2, 10))
+        self.assertIsNone(xkcd_wrapper.Comic._determine_date(missing_one))
+        self.assertIsNone(xkcd_wrapper.Comic._determine_date(missing_all))
+
+    def test__determine_image_extension(self):
+        c = xkcd_wrapper.Comic(test_dict, raw_image=raw_comic_image)
+
+        self.assertEqual(c._determine_image_extension(), 'png')
